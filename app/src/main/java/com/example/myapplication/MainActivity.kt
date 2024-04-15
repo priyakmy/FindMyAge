450bf81801722b400d11da4363dad33d0bc8f3d5
priya Kumari <priyakum1071@gmail.com>
2024-04-15 16:24:35 +0530

    Second Commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,29 +1,32 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import kotlin.android.synthetic.main.activity_main.*
-import java.util.Calendar
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
-
-class MainActivity : AppCompatActivity()
-{
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
-        btGetage.setOnClickListener {
-            userAgeClick()
-        }
+        val etDoB: EditText = findViewById(R.id.etDoB)
+        val btGetAge: Button = findViewById(R.id.btGetAge)
+        val txShowAge: TextView = findViewById(R.id.txShowAge)
 
-        fun userAgeClick() {
-            val userDOB = Intger.parseInt(etDoB.text.toString())
-            val currentYear = Calendar.getInstance().get(Calendar.YEAR)
-            val userAgeInYears = currentYear - userDOB
-            txShowage.text = "Your age is $userAgeInYears years"
-        }
-
+        btGetAge.setOnClickListener {
+            val userDOB = etDoB.text.toString().toIntOrNull()
+            if (userDOB != null) {
+                val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+                val userAgeInYears = currentYear - userDOB
+                txShowAge.text = "Your age is $userAgeInYears years"
+                Log.d("Log Me", "Your age is $userAgeInYears years")
+            } else {
+                txShowAge.text = "Please enter a valid year"
+            }
         }
     }
+}
